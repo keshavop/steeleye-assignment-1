b02fcc3ebff5e693358dd52f842b29f328cfeeed
Keshav Kumar <keshavkumar8122@gmail.com>
2023-08-06 17:31:08 +0530

    Update Card.jsx
    Removed console key error

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({ cardData, title }) => {
     <div className={styles.container}>
       <div className={styles.title}>{title}</div>
       {Object.entries(cardData).map(([k, v]) => (
-        <div className={styles.cell}>
+        <div className={styles.cell} key={k}>
           <div className={styles.value}>{k}</div>
           <div className={styles.value}>{v}</div>
         </div>
